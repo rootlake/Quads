5e7e80206405b8dd29ab73f3dd78b31581c3cd30
rootlake <rootlake@gmail.com>
2017-01-26 09:32:12 -0500

    Cleaned up

--- Quads/ViewController.swift
+++ Quads/ViewController.swift
@@ -10,21 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var lakeBtn: UIButton!
-    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        lakeBtn.widthAnchor
-    }
+           }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func lakeBtn(_ sender: Any) {
-    }
 
 }
 
